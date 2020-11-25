aa89d4ae91950609e9383fe6a4acb9c6d8f1d5b4
Oriana <oriana.nohemi22@gmail.com>
2020-11-25 10:57:20 -0500

    agrega iconos al header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -9,6 +9,10 @@ export class HeaderComponent implements OnInit {
 
   logo = 'assets/images/ransa-logo.png';
 
+  profile = 'assets/images/profile-icon.png';
+
+  notification = 'assets/images/t-icon.png';
+
   constructor() { }
 
   ngOnInit(): void {
