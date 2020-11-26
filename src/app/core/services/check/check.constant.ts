1e10411e8765a6b0fb66cd6541dd3281d00567e3
Oriana <oriana.nohemi22@gmail.com>
2020-11-25 23:54:44 -0500

    incidencias y preguntass

--- src/app/core/services/check/check.constant.ts
+++ src/app/core/services/check/check.constant.ts
@@ -1,17 +1,61 @@
 export const incidences = [
   {
+    typeOfIncident: 'ausentismos',
+    questions: [
+      '¿Cuentas con los registros de asistencia?',
+      '¿Se trata de la jornada y horario regulares?'
+    ]
+  },
+  {
+    typeOfIncident: 'incumplimiento',
+    questions: [
+      '¿Cuentas con la convocatoria a la capacitación (la que debe llevarse a cabo dentro de la jornada de trabajo)?',
+      '¿Se previó las facilidades para participar en la capacitación?'
+    ]
+  },
+  {
+    typeOfIncident: 'seguridad',
+    questions: [
+      '¿La actividad forma parte de las funciones del puesto?',
+      '¿Se cuenta con evidencia de la indicación incumplida?'
+    ]
+  },
+  {
     typeOfIncident: 'desobediencia',
     questions: [
-      'La actividad forma parte de las funciones del puesto',
-      'Se cuenta con evidencia de la indicacion incumplida'
+      '¿La actividad forma parte de las funciones del puesto?',
+      '¿Se cuenta con evidencia de la indicacion incumplida?'
+    ]
+  },
+  {
+    typeOfIncident: 'respeto',
+    questions: [
+      '¿Alguna persona estuvo presente durante la falta de respeto?',
+      'Estas personas son posibles testigos, identifícalos y asegura su declaración',
+      '¿El trabajador ha reconocido la falta de respeto?',
+      'Evidencia este reconocimiento en una declaración'
+    ]
+  },
+  {
+    typeOfIncident: 'recepcion',
+    questions: [
+      '¿Tienes alguna evidencia de la recepción del dinero o dádivas?',
+      '¿Alguna persona estuvo presente durante la entrega de dinero o dádivas?',
+      'Evidencia este reconocimiento en una declaración',
     ]
   },
   {
     typeOfIncident: 'trabajo',
     questions: [
-      'La actividad forma parte de las funciones del puesto',
-      'Se capacito en el trabajo mal realizado al colaborador',
-      'Se cuenta con evidencia de la capacitacion'
+      '¿La actividad forma parte de las funciones del puesto?',
+      '¿Se capacito en el trabajo mal realizado al colaborador?',
+      '¿Se cuenta con evidencia de la capacitacion?'
+    ]
+  },
+  {
+    typeOfIncident: 'trabajo',
+    questions: [
+      '¿Tienes evidencia de la presunta infracción?',
     ]
   }
 ];
