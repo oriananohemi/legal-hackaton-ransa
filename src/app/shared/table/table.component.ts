c5ce9cd556c7e55f2e2035cc7daf0e98b0e1849d
Oriana <oriana.nohemi22@gmail.com>
2020-11-27 13:25:14 -0500

    estilos

--- src/app/shared/table/table.component.ts
+++ src/app/shared/table/table.component.ts
@@ -4,7 +4,6 @@ import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 
-
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
@@ -84,6 +83,9 @@ export class TableComponent implements OnInit, AfterViewInit {
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  download() {
   }
 
 }
